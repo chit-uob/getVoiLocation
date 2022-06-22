18cef6df1b6238b2b1bf21eab46b77098b3b33aa
User <sample@email.com>
2022-06-22 15:24:32 +0100

    added try catch block in main loop to catch problems

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import handle_response
 import make_api_requests
 
 while True:
-    response = make_api_requests.get_scooter_locations()
-    handle_response.store_response(response)
+    try:
+        response = make_api_requests.get_scooter_locations()
+    except:
+        print("Failed to get scooter locations, retrying in 5 seconds")
+        time.sleep(5)
+        continue
+    try:
+        handle_response.store_response(response)
+    except:
+        print("Failed to store response, will skip storing the response")
     time.sleep(60)
